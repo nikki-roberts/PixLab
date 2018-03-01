ba610c3809cf3b6191955c9f493cb7f4b5d2623b
nikki-roberts <robertsni@s.dcsdk12.org>
2018-03-01 12:19:20 -0700

    activity 3 answers and exercises

--- src/PictureExplorer.java
+++ src/PictureExplorer.java
@@ -796,8 +796,10 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
 	 * Test Main. It will explore the beach
 	 */
 	public static void main(String args[]) {
-		Picture pix = new Picture("beach.jpg");
-		pix.explore();
+		Picture pix = new Picture("cat.jpg");
+		Picture smallPix = pix.scale(0.75, 0.75);
+		smallPix.write("smallMyPix.jpg");
+		smallPix.explore();
 	}
 
 }
